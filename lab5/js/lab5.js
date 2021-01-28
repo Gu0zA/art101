aa44ad5f5621e9e00f9b9cb475f9423585afa116
Gu0zA <71299596+Gu0zA@users.noreply.github.com>
2021-01-28 23:12:59 +0800

    lab5

---
+++ lab5/js/lab5.js
@@ -0,0 +1,21 @@
+/*
+ * Author:    Sunny yan & zihao Liu<xyan52@ucsc.edu> <zliu139@ucsc.edu>
+ * Created:   2021/1/27
+ *
+ */
+
+ // Define Varibles
+ make = "ShenZhen Metro";
+ model = "Line 1&Line 5";
+ color = "White";
+ year = 2004;
+
+ // calculate
+ age = 2021 - year
+
+ // Outout
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + " <br>");
+document.writeln("Age: " + age + " years<br>");
