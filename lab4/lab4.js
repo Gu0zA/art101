7dddebbe6722987035fd982c3901cb9f6dd75886
Gu0zA <71299596+Gu0zA@users.noreply.github.com>
2021-02-04 21:01:34 +0800

    Create lab4.js

---
+++ lab4/lab4.js
@@ -0,0 +1,11 @@
+// Print the map before the level
+	//"0" for land
+	//"1" for wall
+	//"2" for box
+	//"3" for destination
+	//"4" for box+destination
+	//"5" for player+destination
+	//"6" for player
+
+//Set up the movement method of the player
+//Set up the movement method of the box
